7c42b3ac7fe382b364381aca4af7276081686f58
Madalin Ignisca <619561+madalinignisca@users.noreply.github.com>
2020-12-11 10:03:53 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,10 @@ terraform {
   }
 }
 
+# Set the variable value in *.tfvars file
+# or using the -var="hcloud_token=..." CLI option
+variable "hcloud_token" {}
+
 # Configure the Hetzner Cloud Provider
 provider "hcloud" {
   token = var.hcloud_token
