ca54844655a79a503ec26f816966af42e244c696
Madalin Ignisca <619561+madalinignisca@users.noreply.github.com>
2020-12-11 10:01:30 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    hcloud = {
+      source = "hetznercloud/hcloud"
+      version = "1.23.0"
+    }
+  }
+}
+
 # Configure the Hetzner Cloud Provider
 provider "hcloud" {
   token = var.hcloud_token
