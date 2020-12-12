becf22f0bee2fb0a27a3105c5b3cec032a4eeef0
Madalin Ignisca <619561+madalinignisca@users.noreply.github.com>
2020-12-12 07:18:43 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,10 +10,17 @@ terraform {
 # Set the variable value in *.tfvars file
 # or using the -var="hcloud_token=..." CLI option
 variable "hcloud_token" {}
+variable "ssh_key" {}
 
 # Configure the Hetzner Cloud Provider
 provider "hcloud" {
   token = var.hcloud_token
+}
+
+# Create a new SSH key
+resource "hcloud_ssh_key" "default" {
+  name = "Terraform Example"
+  public_key = var.ssh_key
 }
 
 # Create a server
@@ -21,4 +28,7 @@ resource "hcloud_server" "terraform101" {
   name = "terraform101.tabaradeweb.ro"
   image = "ubuntu-20.04"
   server_type = "cx11"
+  location = "hel1"
+  datacenter = "hel1-dc2"
+  ssh_keys = [ "default" ]
 }
